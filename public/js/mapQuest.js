08b748b0c9ab45b284a0e364a5bb8c76b4759ceb
Ruiqin Li <r5li@ucsd.edu>
2021-02-25 12:54:00 -0800

    Lab8 finished

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'IkhaUIpAbI3xnWCtrWBZIMow5HvJBKvi';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+  		center: [32.87872751824317, -117.2358645447225],
+  		layers: L.mapquest.tileLayer('map'),
+  		zoom: 12,
+  		zoomControl: false
+	});
+
+	// Add a marker to the map
+	L.marker([32.87872751824317, -117.2358645447225]).addTo(map);
 }
